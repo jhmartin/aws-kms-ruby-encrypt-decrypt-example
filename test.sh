87745117c76f592d50f1e49b6f879c9a946ce8a8
Jason Martin <jhmartin@toger.us>
2017-06-17 16:22:28 -0700

    Add test.sh

---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo FOO > test.input
+bundle exec ruby ./kms-encrypt.rb test.input | tee test.encrypted
+bundle exec ruby ./kms-decrypt.rb test.encrypted | tee test.decrypted
+diff -y test.decrypted test.input
