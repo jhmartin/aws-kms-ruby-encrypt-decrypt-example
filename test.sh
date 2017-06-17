e566b6cd43ae600f41dbbc3987204de10331e4b9
Jason Martin <jhmartin@toger.us>
2017-06-17 16:50:35 -0700

    Gemnasium

--- test.sh
+++ test.sh
@@ -8,4 +8,5 @@ diff -y test.decrypted test.input || exit 1
 # Signal to gemnasium that test runs properly.
 curl -LsO https://github.com/gemnasium/toolbelt/releases/download/0.2.9/gemnasium_0.2.9_linux_amd64.tar.gz
 tar  --strip-components=1 -xvzf gemnasium_0.2.9_linux_amd64.tar.gz gemnasium_0.2.9_linux_amd64/gemnasium
-./gemnasium autoupdate github.com/jhmartin/aws-kms-ruby-encrypt-decrypt-example true
+
+REVISION=$CIRCLE_SHA1 ./gemnasium autoupdate github.com/jhmartin/aws-kms-ruby-encrypt-decrypt-example true
