7254025c8335716712cd4ee9307d472b211268a7
Jason Martin <jhmartin@toger.us>
2017-06-17 17:36:50 -0700

    Apply rubocop fixes

--- kms-decrypt.rb
+++ kms-decrypt.rb
@@ -7,18 +7,16 @@ require 'gibberish'
 require 'json'
 
 if ARGV[0].nil?
-  puts "Usage: kms-decrypt.rb INPUTFILE"
+  puts 'Usage: kms-decrypt.rb INPUTFILE'
   exit 1
 end
 
-
 contents = JSON.parse(IO.read(ARGV[0]))
-kms = Aws::KMS::Client.new(region:'us-east-1')
-datakey =  Base64.strict_decode64(contents['datakey'])
+kms = Aws::KMS::Client.new(region: 'us-east-1')
+datakey = Base64.strict_decode64(contents['datakey'])
 
-cleartextkey = kms.decrypt(:ciphertext_blob => datakey,
-  encryption_context: { 'KeyType' => 'Some descriptive text here' }
-)
+cleartextkey = kms.decrypt(ciphertext_blob: datakey,
+                           encryption_context: { 'KeyType' => 'Some descriptive text here' })
 
 cipher = Gibberish::AES.new(cleartextkey.plaintext)
 cleartext = cipher.decrypt(contents['ciphertext'])
