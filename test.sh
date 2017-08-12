f8e6ece5578112e587911c64a74098173d54e8cf
Jason Martin <jhmartin@toger.us>
2017-08-12 16:19:15 -0700

    Migrate to Circle 2.0

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -x
+set -ex
 echo FOO > test.input
 bundle exec ruby ./kms-encrypt.rb test.input | tee test.encrypted
 bundle exec ruby ./kms-decrypt.rb test.encrypted | tee test.decrypted
